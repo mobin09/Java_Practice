61d682ac0a2529f0181fe09ca3eeabba7fed1235
Mobin Arshad <mobink09@gmail.com>
2022-12-06 13:53:37 +0530

    get and set name of thread

---
+++ multithreading/Geting_SettingName.java
@@ -0,0 +1,33 @@
+package multithreading;
+
+
+class MyThread4 extends Thread{
+	@Override
+	public void run() {
+		String name = Thread.currentThread().getName();
+		System.out.println("run() is executed by " + name);
+	}
+}
+
+public class Geting_SettingName {
+
+	public static void main(String[] args) {
+		System.out.println(Thread.currentThread());
+		String name = Thread.currentThread().getName();
+		System.out.println("main() is executed by thread named: " + name);
+		
+		MyThread4 t = new MyThread4();
+		t.start();
+		System.out.println("Name of  Thread of MyThread4 class: " +t.getName());
+		
+		Thread.currentThread().setName("Mobin");
+		
+		System.out.println(10/0);
+		
+		
+		
+		
+
+	}
+
+}
