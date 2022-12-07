84b6bbc5134a40441a847ac5c56c7ecc4a3d1938
Mobin Arshad <mobink09@gmail.com>
2022-12-07 14:23:04 +0530

    added Interrupted Exception

---
+++ multithreading/InterruptedExceptionDemo.java
@@ -0,0 +1,29 @@
+package multithreading;
+
+class MyThread9 extends Thread{
+	@Override
+	public void run() {
+		for(int i=0; i<5; i++) {
+			System.out.println("Child Thread");
+			try {
+				Thread.sleep(3000);
+			}catch(InterruptedException e) {
+				System.out.println("I got interrupted");
+			}
+			
+		}
+	}
+}
+
+
+public class InterruptedExceptionDemo {
+
+	public static void main(String[] args) {
+		 MyThread9 t = new MyThread9();
+		 t.start();
+		 
+		 System.out.println("End of Main Thread");
+        
+	}
+
+}
