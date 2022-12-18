77e243717d4ec64ddfbc1031170c51add06575ba
Mobin Arshad <mobink09@gmail.com>
2022-12-18 18:01:34 +0530

    Added user defined Exception

--- exceptionHandling/ThrowKeywordExample.java
+++ exceptionHandling/ThrowKeywordExample.java
@@ -1,5 +1,9 @@
 package exceptionHandling;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class ThrowKeywordExample {
 
 	static void withoutThrow() {
@@ -15,17 +19,53 @@ public class ThrowKeywordExample {
 	
     static void checkEligibility(int age) {
     	if(age < 18) {
-    		throw new ArithmeticException("Invalid Age");
+    		throw new ArithmeticException("Invalid Age"); // Unchecked Exception
     	} else {
     		System.out.println("Eligible for the vote !!");
     	}
     } 
+    
+    // Checked Exception
+    public static void method() throws FileNotFoundException {
+    	FileReader file = new FileReader("abc.txt");
+    	BufferedReader br = new BufferedReader(file);
+    	
+    	throw new FileNotFoundException();
+    	
+    }
 	
 	public static void main(String[] args) {
 		
 		//withoutThrow();
 		//withThrow();
-		checkEligibility(13);
+		//checkEligibility(13);
+//		try {
+//			method();
+//		}catch(FileNotFoundException e) {
+//			e.printStackTrace();
+//			
+//		}
+		
+		try {
+		throw new UserDefinedException("User Defined Exception is Raised");
+		}catch(UserDefinedException e) {
+			String s =  e.getMessage();
+			System.out.println(s);
+		}
+		
+		
 	}
 
 }
+
+// Throwing user defined Exception
+
+class UserDefinedException extends Exception{
+	
+	public UserDefinedException(String str) {
+		super(str); // calling Constructor of parent exception
+	}
+}
+
+
+
