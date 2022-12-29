ffd50c9412720eba905a97bc77c9f2cfa31e6c2a
Mobin Arshad <mobink09@gmail.com>
2022-12-29 23:30:42 +0530

    demo

---
+++ basics01/Demo2.java
@@ -0,0 +1,13 @@
+package basics01;
+
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		int Runnable  = 10;
+        System.out.println(Runnable);
+        int Integer = 10;
+        System.out.println(Integer);
+	}
+
+}
