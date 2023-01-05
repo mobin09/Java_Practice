7b439dcbd0ddbb7a0a51d85121ecd08204fb3c21
Mobin Arshad <mobink09@gmail.com>
2023-01-05 20:37:00 +0530

    added the File constructors and file/directory

--- javaIo/Demo1.java
+++ javaIo/Demo1.java
@@ -7,9 +7,22 @@ public class Demo1 {
 
 	public static void main(String[] args) throws IOException {
 		File f = new File("abc.txt");
-		System.out.println(f.exists());
+		//System.out.println(f.exists());
 		f.createNewFile();
-		System.out.println(f.exists());
+		//System.out.println(f.exists());
+		
+		File abc = new File("mobin");
+		//System.out.println(abc.exists());
+		abc.mkdir();
+		//System.out.println(abc.exists());
+		
+		File m  = new File("Mobin123");
+		m.mkdir();
+		File a = new File(m, "demo.txt");
+		a.createNewFile();
+		
+		System.out.println("Done");
+		
 
 	}
 
