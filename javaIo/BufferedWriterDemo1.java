fd403131c9864be88e0549ea0eb8da6a3433fa92
Mobin Arshad <mobink09@gmail.com>
2023-01-10 14:25:05 +0530

    Added the BufferedWriter

---
+++ javaIo/BufferedWriterDemo1.java
@@ -0,0 +1,29 @@
+package javaIo;
+
+import java.io.*;
+
+public class BufferedWriterDemo1 {
+
+	public static void main(String[] args) {
+		try {
+		FileWriter fw = new FileWriter("abc1.txt");
+		BufferedWriter bw = new BufferedWriter(fw);
+		bw.write(65);
+		bw.newLine();
+		char[] ch = {'A', 'B', 'C'};
+		bw.write(ch);
+		bw.newLine();
+		bw.write("Mobin");
+		bw.newLine();
+		bw.write("Arshad Khan");
+		bw.newLine();
+		
+		bw.flush();
+		bw.close();
+		
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
