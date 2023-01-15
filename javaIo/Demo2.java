8085c682981cd5de741222450f4c2945664d811b
Mobin Arshad <mobink09@gmail.com>
2023-01-15 19:42:21 +0530

    Show list

--- javaIo/Demo2.java
+++ javaIo/Demo2.java
@@ -16,6 +16,33 @@ public class Demo2 {
 		f2.mkdir();
 		System.out.println(f2.exists());
 		
+		File f3 = new File(f2, "DelhiIndians.txt");
+		f3.createNewFile();
+		File f4 = new File(f2, "MumbaiIndians.txt");
+		f4.createNewFile();
+		
+		System.out.println("File is created " + f3.exists() + " " + f4.exists());
+		
+		File f5 = new File("E:\\");
+		String[] str = f5.list();
+		
+		for(int i=0; i<str.length; i++) {
+			File f6 = new File(f5, str[i]);
+		    
+			if(f6.isFile()) {
+				System.out.println("This is the File: " + str[i]);
+			}
+			
+			if(f6.isDirectory()) {
+				System.out.println("This is the Directory: " + str[i]);
+			}
+			
+			
+		}
+		
+		System.out.println("THIS IS THE END IF THE DIKENBD");
+		
+		
 	}
 
 }
