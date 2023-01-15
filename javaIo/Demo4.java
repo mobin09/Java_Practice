3314c0fd0a2c6488b1a295ee54fa45bd2216754f
Mobin Arshad <mobink09@gmail.com>
2023-01-15 21:39:07 +0530

    Demo4 added

---
+++ javaIo/Demo4.java
@@ -0,0 +1,23 @@
+package javaIo;
+
+import java.io.*;
+
+public class Demo4 {
+
+	public static void main(String[] args) throws IOException {
+		FileWriter fw = new FileWriter("abc.txt");
+		fw.write(97);
+		fw.write("\n");
+		fw.write("Enjoying to write the code");
+		fw.write("\n");
+		char[] ch = {'M', 'O', 'B', 'I', 'N'};
+		fw.write(ch);
+		
+		fw.close();
+		
+		System.out.println("Open abc.txt to see the result");
+		
+        
+	}
+
+}
