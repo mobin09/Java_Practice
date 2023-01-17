41a4a68858e0c7c1b598f3c109572db03ec3fa34
Mobin Arshad <mobink09@gmail.com>
2023-01-17 21:56:35 +0530

    Serialization

---
+++ serializationEx/Demo1.java
@@ -0,0 +1,33 @@
+package serializationEx;
+
+
+import java.io.*;
+
+
+class Dog implements Serializable{
+	int i=10;
+	int j =20;
+}
+
+public class Demo1 {
+
+	public static void main(String[] args)  throws IOException, ClassNotFoundException{
+		Dog d1 = new Dog();
+		FileOutputStream fos = new FileOutputStream("abc.txt");
+		ObjectOutputStream oos = new ObjectOutputStream(fos);
+		oos.writeObject(d1);
+		
+		oos.flush();
+		oos.close();
+		
+		
+		FileInputStream fis = new FileInputStream("abc.txt");
+		ObjectInputStream ois = new ObjectInputStream(fis);
+		Dog d2 =  (Dog)ois.readObject();
+		System.out.println(d2.i+ "......" + d2.j);  
+		
+		
+
+	}
+
+}
