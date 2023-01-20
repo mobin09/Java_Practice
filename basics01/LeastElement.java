c8b3d20b1d1ca7ebf5f28d39d7083ecbca664260
Mobin Arshad <mobink09@gmail.com>
2023-01-20 23:51:59 +0530

    Test

---
+++ basics01/LeastElement.java
@@ -0,0 +1,24 @@
+package basics01;
+
+public class LeastElement {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 4;
+		int c =1;
+		
+		if(a<b) {
+		    if(a < c) {
+		       System.out.println("A is least");	
+		    }else {
+		    	System.out.println("C is least");
+		    }
+		}else if( b < a) {
+			System.out.println("B is least");
+		}
+		
+		
+
+	}
+
+}
