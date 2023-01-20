7fbc2ef0199ba4a843cdedccbca6ca74624af469
Mobin Arshad <mobink09@gmail.com>
2023-01-21 00:28:59 +0530

    Snipits

---
+++ basics01/Demo5.java
@@ -0,0 +1,14 @@
+package basics01;
+
+public class Demo5 {
+
+	public static void main(String[] args) {
+		args[1] = "Arshad";
+		System.out.println(args[0] + " " + args[1]);
+		
+		//case1: Supose passing Abc Def  command line ... what is output 
+		//case2: Suppose passing Abc from  command line ... what is output
+		
+	}
+
+}
