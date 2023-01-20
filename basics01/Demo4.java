98322de954424d9ac02245af14c30f76ebf18081
Mobin Arshad <mobink09@gmail.com>
2023-01-20 22:44:10 +0530

    SIZE, MIN_VALUE, MAX_VALUE --> constants

---
+++ basics01/Demo4.java
@@ -0,0 +1,18 @@
+package basics01;
+
+public class Demo4 {
+
+	public static void main(String[] args) {
+		// checking SIZE, MIN_VALUE, MAX_VALUE for the Byte classs
+		System.out.println(Byte.SIZE);
+		System.out.println(Byte.MIN_VALUE);
+		System.out.println(Byte.MAX_VALUE);
+		
+		// checking the SIZE, MAX_VALUE and MIN_VALUE for the Integer class
+		System.out.println(Integer.SIZE);
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(Integer.MAX_VALUE);
+
+	}
+
+}
