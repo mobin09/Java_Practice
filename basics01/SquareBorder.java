5c20f9416580c1b7bb0f7de646a5596ccbc2ddcc
Mobin Arshad <mobink09@gmail.com>
2023-01-22 19:38:33 +0530

    Square Border Pattern

---
+++ basics01/SquareBorder.java
@@ -0,0 +1,26 @@
+package basics01;
+
+public class SquareBorder {
+
+	public static void main(String[] args) {
+		int n = 10;
+
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				if(i==0 || i== n-1 || j== 0 || j == n-1) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			
+			System.out.println();
+			
+		}
+			    
+			
+			    
+		
+	}
+
+}
