ec753dcd04d8027f4c957cdae21440636e2c300c
Mobin Arshad <mobink09@gmail.com>
2023-01-22 21:08:24 +0530

    T pattern

---
+++ basics01/Tpattern.java
@@ -0,0 +1,21 @@
+package basics01;
+
+public class Tpattern {
+
+	public static void main(String[] args) {
+		int n = 5;
+		for(int i=0; i<n; i++) {
+			for(int j = 0; j<n; j++) {
+				if(i==0 || j==n/2) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			
+			System.out.println();
+			
+		}
+	}
+
+}
