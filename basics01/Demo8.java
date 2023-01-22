71c96e08f4b3064aaf0a4778d8d04d019ceb9daf
Mobin Arshad <mobink09@gmail.com>
2023-01-22 22:39:01 +0530

    __

---
+++ basics01/Demo8.java
@@ -0,0 +1,17 @@
+package basics01;
+
+public class Demo8 {
+
+	public static void main(String[] args) {
+		int x = 5____0; // valid
+		System.out.println(x); //50
+		//int y = _____50; invalid
+		//int z = 50_____; // invalid
+		float f = 123.76_86f;
+		double d = 1_2_3_4;
+		
+		
+
+	}
+
+}
