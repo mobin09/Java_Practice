5406691896e68be2e0fcc620adf129a3a854759f
Mobin Arshad <mobink09@gmail.com>
2023-01-23 22:09:43 +0530

    NPattern

---
+++ basics01/NPattern.java
@@ -0,0 +1,24 @@
+package basics01;
+
+public class NPattern {
+
+	public static void main(String[] args) {
+		
+		int n=10;
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				if(i==j || j==0 || j== n-1) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			
+			System.out.println();
+			
+		}
+		
+
+	}
+
+}
