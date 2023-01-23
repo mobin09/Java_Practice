96e01d010e8a178787e690f965e2f445363fa1cc
Mobin Arshad <mobink09@gmail.com>
2023-01-23 23:35:53 +0530

    Basics logic

---
+++ basics01/Demo9.java
@@ -0,0 +1,29 @@
+package basics01;
+
+public class Demo9 {
+
+	public static void main(String[] args) {
+		int x = 10;
+		if(++x < 10 && (x/0)>10) {
+			System.out.println("Hello");
+		}else {
+			System.out.println("Hi");
+		}
+		
+		int i = 10;
+		int j=20;
+		int k = (j +=i)/5;
+		System.out.println("i:"+ i + " j:" + j +" k:"+k);
+		
+		
+		boolean b = false;
+		if(b =true) {
+			System.out.println("Hello");
+		}else {
+			System.out.println("World");
+		}
+		
+
+	}
+
+}
