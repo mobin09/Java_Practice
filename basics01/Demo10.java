ef6994f4d72a4a79c5da376b243bc5f834a6ce80
Mobin Arshad <mobink09@gmail.com>
2023-01-24 21:29:00 +0530

    Simple Pattern

---
+++ basics01/Demo10.java
@@ -0,0 +1,18 @@
+package basics01;
+
+public class Demo10 {
+
+	public static void main(String[] args) {
+		int n=5;
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=n; j++) {
+				System.out.print(i);
+			}
+			
+			System.out.println();
+			
+		}
+
+	}
+
+}
--- basics01/Demo9.java
+++ basics01/Demo9.java
@@ -24,6 +24,9 @@ public class Demo9 {
 		}
 		
 
+		if(true);
+		    int c = 5;	
+		
 	}
 
 }
