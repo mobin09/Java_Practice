8575f53a79a865ccd1a87c9c1f9f89eced35819e
Mobin Arshad <mobink09@gmail.com>
2023-01-27 21:46:49 +0530

    _

--- opps/IsaRel.java
+++ opps/IsaRel.java
@@ -5,12 +5,24 @@ class P{
 	public void m1() {
 		System.out.println("Parent");
 	}
+	
+	public void m3() {
+		System.out.println("This method will be override  by the child");
+	}
+	
 }
 
 class Child extends P{
 	public void m2() {
 		System.out.println("Child");
 	}
+	
+	@Override
+	public void m3() {
+		System.out.println("Override method");
+	}
+	
+	
 }
 
 
@@ -25,8 +37,9 @@ public class IsaRel {
 		c.m1();
 		c.m2();
 		
-	   P p1 = new P();
+	   P p1 = new Child();
 	   p1.m1();
+	   p1.m3();
 	   //p1.m2();// CE:
 	   
 	   //Child c = new P();
