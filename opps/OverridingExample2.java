5cd9fdcdcce1a415d520ef5b2b81a47224f8846e
Mobin Arshad <mobink09@gmail.com>
2023-01-30 22:08:28 +0530

    Overrding Exm 2

---
+++ opps/OverridingExample2.java
@@ -0,0 +1,27 @@
+package opps;
+
+
+class P2 {
+	
+	public Object m1() {
+		return "Hello";
+	}
+	
+}
+
+class C2 extends P2{
+	
+	public String  m1() {
+		return " ";
+	}
+	
+}
+
+public class OverridingExample2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
