7c8d632c8117de6491a5ccc1f8f263f7f54571f0
Mobin Arshad <mobink09@gmail.com>
2023-01-31 22:13:43 +0530

    Overriding Exampl4

---
+++ opps/OverrideExample4.java
@@ -0,0 +1,24 @@
+package opps;
+
+
+class P5{
+	public static void m1() {
+		System.out.println("Class level method");
+	}
+}
+
+class C5  extends P5{
+//	public void m1() {      // we will get compile time issues
+//		System.out.println("Overriding of m1");
+//	}
+}
+
+
+public class OverrideExample4 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
