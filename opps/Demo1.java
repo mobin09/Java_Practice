d62f1f1520a37949cbfe364cfe2304e6e6ec6f88
Mobin Arshad <mobink09@gmail.com>
2023-02-03 21:46:14 +0530

    Demo

---
+++ opps/Demo1.java
@@ -0,0 +1,19 @@
+package opps;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// Parent reference can be used to hold child object
+		Object o = new String("Mobin Arshad");
+		// Interface reference can be used to hold implemented class
+		Runnable r = new Thread();
+		
+		System.out.println(o);
+		
+		
+		
+		
+
+	}
+
+}
