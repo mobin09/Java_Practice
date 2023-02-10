a7631e580dd9175b741f8ac088c695a6b74f1431
Mobin Arshad <mobink09@gmail.com>
2023-02-10 21:56:07 +0530

    Static methods Example

--- opps/StaticBlock.java
+++ opps/StaticBlock.java
@@ -1,5 +1,10 @@
 package opps;
 
+// From 1.7 version onwards main() method is mandatory
+// to start a program execution Hence from 1.7V onwards without writing main()
+// method it is impossible to print some statements to the console
+
+
 // This Example is for practice and learning 
 
 public class StaticBlock {
