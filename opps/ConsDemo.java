1c0e1ab97d2ea97b64e38603f1b0ea25db9404cf
Mobin Arshad <mobink09@gmail.com>
2023-02-13 22:18:46 +0530

    Constructor w.r.t inheritance

---
+++ opps/ConsDemo.java
@@ -0,0 +1,20 @@
+package opps;
+
+class Cons {
+	Cons(int i) {
+
+	}
+
+	Cons() {
+	}
+
+}
+
+public class ConsDemo extends Cons {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
