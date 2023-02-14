574732a522449d808ccb82b31cbcb5d27f97e775
Mobin Arshad <mobink09@gmail.com>
2023-02-14 23:03:57 +0530

    Misc..

---
+++ opps/Exmp.java
@@ -0,0 +1,25 @@
+package opps;
+
+
+
+class Exmp1{
+	
+	
+}
+
+class Exmp2{
+	
+}
+
+class Exmp3{
+	
+}
+
+public class Exmp {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+	
+
+}
