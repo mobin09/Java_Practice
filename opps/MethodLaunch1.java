bd538ecab81b09b1f2dc083664684a03305a78f8
Mobin Arshad <mobink09@gmail.com>
2023-02-18 21:22:54 +0530

    Methods

---
+++ opps/MethodLaunch1.java
@@ -0,0 +1,23 @@
+package opps;
+
+
+class Calculator1{
+	int a,b,c;
+	void add() {
+		a = 10;
+		b= 20;
+		c = a+b;
+		System.out.println(c);
+				
+	}
+}
+
+public class MethodLaunch1 {
+
+	public static void main(String[] args) {
+		Calculator1 calc = new Calculator1();
+		calc.add();
+
+	}
+
+}
