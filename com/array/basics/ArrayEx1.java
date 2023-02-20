56e22f5bd55ae72bea20c0337e1c5fd3c8c11e82
Mobin Arshad <mobink09@gmail.com>
2023-02-20 21:57:19 +0530

    Array 1

--- com/array/basics/ArrayEx1.java
+++ com/array/basics/ArrayEx1.java
@@ -3,7 +3,24 @@ package com.array.basics;
 public class ArrayEx1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Store marks of student for 10 subjects
+		// declaring the array and assigning the value to it
+		
+		int[] subjects = new int[10];
+		subjects[0] = 88;
+		subjects[1] = 65;
+		subjects[2] = 70;
+		subjects[3]  = 65;
+		subjects[4] = 81;
+		subjects[5] = 75;
+		subjects[6] = 81;
+		subjects[7] = 62;
+		subjects[8] = 100;
+		subjects[9] = 92;
+		
+		System.out.println(subjects[6]); // accessing the value of the array
+		
+		
 
 	}
 
