2363f36f936319067c33db93289a4b67907330c5
Mobin Arshad <mobink09@gmail.com>
2023-02-21 11:04:08 +0530

    snippets

---
+++ opps/SnippetsSession1.java
@@ -0,0 +1,45 @@
+package opps;
+
+public class SnippetsSession1 {
+
+	public static void main(String[] args) {
+		//1.
+//		while(true) {
+//			System.out.println("Hello");
+//		}
+		
+		//System.out.println("Hii"); // unreachable code
+		
+		//2.
+		
+//		while(false) {
+//			//System.out.println("Hi");
+//		}
+		
+		//System.out.println("Hello");
+		
+//		while(true) {
+//			int x = 10;
+//		}
+
+		
+//	final int a = 10, b=20;
+//		
+//		while(a<b) {
+//			System.out.println("Hello");
+//		}
+//		
+//		System.out.println("Hiii");
+//		
+//		do
+//			System.out.println("Hello");
+//		while(true);
+//		
+//	  do while(true)
+//		  System.out.println("Hello");
+//	while(true);
+		
+		
+	}
+
+}
