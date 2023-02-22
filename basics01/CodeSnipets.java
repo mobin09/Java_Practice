97aa886373b5beafe880c81bf3ee492b04e7e6a5
Mobin Arshad <mobink09@gmail.com>
2023-02-22 12:53:17 +0530

    Snipets session

---
+++ basics01/CodeSnipets.java
@@ -0,0 +1,48 @@
+package basics01;
+
+public class CodeSnipets {
+
+	public static void main(String[] args) {
+	
+		//1.
+//		do {
+//			System.out.println("Hello");
+//		}while(true);
+//		
+//		System.out.println("hi"); // unreachable line
+
+		//2.
+//	 int a=10, b =20;
+//		do {
+//			System.out.println("Hello1");
+//		}while(a<b);
+//		
+//		System.out.println("hii");
+//		//3.
+//		final int a=10, b =20;
+//		do {
+//			System.out.println("Helo");
+//		}while(a<b);
+//		
+//		System.out.println("Hiiii"); // unreachable statement
+//	    //4.
+//		int  k=0, j=0;
+//		int i=0, Bollean b = true;
+//		int m=0, int n=0;
+//		
+		//5.0
+//		int i=0;
+//		for(System.out.println("Hello u are sleeping"); i<2; i++) {
+//		    System.out.println("no boss, you are sleeping");
+//		}
+		//6.0
+		
+		int i=0;
+		for(System.out.println("Hello"); i<3; System.out.println("Hi")) {
+			i++;
+		}
+		
+		
+
+}
+}
