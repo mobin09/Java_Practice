2df8cbc4a2a52fb2a2104a9a696e10c88726dc62
Mobin Arshad <mobink09@gmail.com>
2023-02-28 22:23:19 +0530

    StringBuilder and StringBuffer

--- string/Test1.java
+++ string/Test1.java
@@ -16,7 +16,22 @@ class Test1 {
 	   StringBuilder sb = new StringBuilder("Mobin");
 	   sb.append("Arshad");
 	   
+	   String s2 = "Abc";
+	   String s3 = new String("Abc");
+	   System.out.println(s2 == s3);
+	   System.out.println(s2.equals(s3));
+	  
+	   StringBuffer sb1 = new StringBuffer("Mobin");
+	   StringBuffer sb2 = new StringBuffer("Mobin");
+	   System.out.println("StringBuffer");
+	   System.out.println(sb1 == sb2);
+	   System.out.println(sb1.equals(sb2));
 	   
+	   StringBuilder sb3 = new StringBuilder("Mobin");
+	   StringBuilder sb4 = new StringBuilder("Mobin");
+	   System.out.println("StringBuilder");
+	   System.out.println(sb3 == sb4);
+	   System.out.println(sb3.equals(sb4));
 	   
 	   
 	}
