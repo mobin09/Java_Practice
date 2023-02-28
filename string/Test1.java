96d1742dd49c999c295394870d1468cabde70298
Mobin Arshad <mobink09@gmail.com>
2023-02-28 21:53:08 +0530

    String basics

---
+++ string/Test1.java
@@ -0,0 +1,24 @@
+package string;
+
+class Test1 {
+
+	public static void main(String[] args) {
+	   String s = "Hello I am String";
+	   String s1 = new String("String can also be created like this");
+	   System.out.println(s1);
+	   System.out.println(s);
+	   
+	   String name = "Mobin"; // in name is Mobin
+	    name.concat("Arshad");// concat
+	   
+	   System.out.println(name);
+	   
+	   StringBuilder sb = new StringBuilder("Mobin");
+	   sb.append("Arshad");
+	   
+	   
+	   
+	   
+	}
+
+}
