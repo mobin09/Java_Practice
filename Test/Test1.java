31c2d9255c8ce4ed879d7d9d6bfd187ae57f3040
Mobin Arshad <mobink09@gmail.com>
2023-03-05 12:20:36 +0530

    Testing

---
+++ Test/Test1.java
@@ -0,0 +1,19 @@
+package Test;
+
+class Test1 {
+
+	public static void main(String[] args) {
+
+	
+		String[] str = { "mon", "tue", "wed" };
+		for (String S : str) {
+			switch (S) {
+			case ("mon"):
+				System.out.println("Hii");
+		
+	      }
+     }
+
+}
+
+}
