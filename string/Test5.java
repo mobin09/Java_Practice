72674dd4f5bdf78527c42219767071eef8a77b75
Mobin Arshad <mobink09@gmail.com>
2023-03-10 22:10:49 +0530

    StringBuffer

---
+++ string/Test5.java
@@ -0,0 +1,14 @@
+package string;
+
+class Test5 {
+
+	public static void main(String[] args) {
+		final StringBuffer sb = new StringBuffer("Mobin");
+		sb.append(" Arshad");
+		System.out.println(sb);
+		
+		//sb = new StringBuffer("Abcd"); // here we will get compile time issues 
+
+	}
+
+}
