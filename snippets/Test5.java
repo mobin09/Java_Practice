53daf5f98eac8f3b06d005120fc59327f7e24ca2
Mobin Arshad <mobink09@gmail.com>
2023-03-11 14:48:38 +0530

    String snippets

---
+++ snippets/Test5.java
@@ -0,0 +1,21 @@
+package snippets;
+
+class Test5 {
+
+	public static void main(String[] args) {
+		String s = "Mobin";
+		String s1 = new String("Abcd");
+		s = s1;
+		
+		//System.out.println(s);
+		//System.out.println(s.equalsIgnoreCase(s));
+		
+		
+		System.out.println(""=="");
+		System.out.print("");
+		System.out.println("A"=="A");
+		System.out.println(s=s1);
+
+	}
+
+}
