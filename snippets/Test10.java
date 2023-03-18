6129193037f15d00568c748d28eef772a9455163
Mobin Arshad <mobink09@gmail.com>
2023-03-19 00:37:17 +0530

    Snippets

---
+++ snippets/Test10.java
@@ -0,0 +1,18 @@
+package snippets;
+
+ class Test10 {
+
+	public static void main(String[] args) {
+		boolean flag1 = true;
+		boolean flag2 = false;
+		boolean flag3 = true;
+		boolean flag4 = false;
+		
+		System.out.println(!flag1 == flag2 != flag3 == !flag4);
+		System.out.println(flag1 = flag2 != flag3 == !flag4);
+		
+		
+		
+	}
+
+}
