8d51763711be9bc3671216a194b52fd6e7a58eb1
Mobin Arshad <mobink09@gmail.com>
2023-03-18 19:51:52 +0530

    Snippets

---
+++ snippets/Test7.java
@@ -0,0 +1,16 @@
+package snippets;
+
+ class Test7 {
+
+	 String[] x;
+	 int[] a[] = {{1,2}, {1}};
+	 Object c = new long[4];
+	 Object[] d = x;
+	  
+	 
+	public static void main(String[] args) {
+		System.out.println("Compile Succesfully");
+
+	}
+
+}
