d866f9825edad1a3d6a901d83012c6298f1644c0
Mobin Arshad <mobink09@gmail.com>
2023-03-26 21:31:58 +0530

    Snippets

--- areaOfGeometry/LaunchArea.java
+++ areaOfGeometry/LaunchArea.java
@@ -2,31 +2,17 @@ package areaOfGeometry;
 
 public class LaunchArea {
 
-	 static boolean isAvailable(int x) {
-		 return x-- > 0 ? true: false;
-	 }
-	
 	public static void main(String[] args) {
-		int x = 5;
-		while(isAvailable(x)) {
-			System.out.println(x--);
-		}
-		
-		
-		CallCulateArea area =new CallCulateArea();
-		
+
+		CallCulateArea area = new CallCulateArea();
+
 		Circle c = new Circle();
 		Rectangle r = new Rectangle();
 		Square s = new Square();
-		
+
 		area.calulateShapeArea(s);
 		area.calulateShapeArea(c);
 		area.calulateShapeArea(r);
-		
-		
-		
-		
-		
-		
+
 	}
 }
--- snippets/Test14.java
+++ snippets/Test14.java
@@ -2,15 +2,22 @@ package snippets;
 
 class Test14 {
 
-	 static boolean isAvailable(int x) {
-		 return x-- > 0 ? true: false;
-	 }
-	
+	static boolean isAvailable(int x) {
+		return x-- > 0 ? true : false;
+	}
+
 	public static void main(String[] args) {
 		int x = 5;
-		while(isAvailable(x)) {
+		while (isAvailable(x)) {
 			System.out.println(x--);
 		}
+		
+		StringBuilder sb = new StringBuilder("Java");
+		String s1 = sb.toString();
+		String s2 = "Java";
+		System.out.println(s1 == s2);
+		
+		
 	}
 
 }
