cde530945b552f51b105d67c109ffd9d3df1cab5
Mobin Arshad <mobink09@gmail.com>
2023-04-08 00:25:26 +0530

    .

--- opps/LaunchLRIr.java
+++ opps/LaunchLRIr.java
@@ -1,6 +1,10 @@
 package opps;
 
 
+abstract class AA{
+ 	abstract public void m1();
+}
+
 interface Left{
 	int x = 888;
 }
@@ -9,14 +13,20 @@ interface Right {
 	int x = 999;
 }
 
-public class LaunchLRIr implements Left, Right {
-
+public class LaunchLRIr extends AA implements Left, Right {
+    public void m1() {
+    	System.out.println("Reference can be created for abstract class");
+    }
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World");
 		System.out.println(Left.x);
 		System.out.println(Right.x);
-
+        AA a = new LaunchLRIr();
+        a.m1();
+        
+        Left c = null;
+        
 	}
 
 }
