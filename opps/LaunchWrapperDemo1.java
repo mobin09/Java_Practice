df34beeca2268a088b0c7937f3f51534a332a42b
Mobin Arshad <mobink09@gmail.com>
2023-04-08 01:36:37 +0530

    Wrapper class constructor deprecated from jdk9

---
+++ opps/LaunchWrapperDemo1.java
@@ -0,0 +1,15 @@
+package opps;
+
+public class LaunchWrapperDemo1 {
+
+	public static void main(String[] args) {
+		//1. Integer
+		Integer i = new Integer(10); // deprecated from jdk 9
+		System.out.println(i);
+		System.out.println(i.hashCode());
+		Boolean b = new Boolean("true"); // deprecated from jdk 9
+		
+
+	}
+
+}
