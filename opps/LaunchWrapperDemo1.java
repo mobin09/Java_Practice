d54e9a035f878f22dc1da5a1b81758a0b5ea306c
Mobin Arshad <mobink09@gmail.com>
2023-04-08 14:24:57 +0530

    Wrapper class

--- opps/LaunchWrapperDemo1.java
+++ opps/LaunchWrapperDemo1.java
@@ -4,12 +4,38 @@ public class LaunchWrapperDemo1 {
 
 	public static void main(String[] args) {
 		//1. Integer
-		Integer i = new Integer(10); // deprecated from jdk 9
-		System.out.println(i);
-		System.out.println(i.hashCode());
-		Boolean b = new Boolean("true"); // deprecated from jdk 9
+//		Integer i = new Integer(10); // deprecated from jdk 9
+//		System.out.println(i);
+//		System.out.println(i.hashCode());
+//		Boolean b = new Boolean("true"); // deprecated from jdk 9
+//		
+		Integer x  = Integer.valueOf(10);
+		x = Integer.valueOf("20");
 		
-
+		System.out.println(x);
+		
+		Boolean b = Boolean.valueOf("Mobin");
+		
+		b = Boolean.valueOf("TRuE");
+		b = Boolean.valueOf(true);
+		b = Boolean.valueOf("true");
+				System.out.println(b);
+				
+				
+		x = Integer.valueOf("1011011",35 );
+		System.out.println(x);
+		System.out.println("Radix defined in the Character class");
+		System.out.println(Character.MAX_RADIX);
+		System.out.println(Character.MIN_RADIX);
+		
+		String c = "true";
+		boolean c1 = Boolean.parseBoolean(c);
+		System.out.println(c1);
+		
+		
+		
+      
+		
 	}
 
 }
