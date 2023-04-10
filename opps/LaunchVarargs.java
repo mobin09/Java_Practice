fbc740c33260a5f4a92a62bcddc9c57fe74cb9a0
Mobin Arshad <mobink09@gmail.com>
2023-04-10 22:21:35 +0530

    var-args

--- opps/LaunchVarargs.java
+++ opps/LaunchVarargs.java
@@ -3,14 +3,36 @@ package opps;
 public class LaunchVarargs {
 
 	public void add(int ...x) {
-		System.out.println("Addition of Numbers");
+		for(int ele: x) {
+			System.out.print(ele+" ");
+		}
+		System.out.println();
+	}
+	
+	
+	public void addInt(int ...x) {
+		int sum = 0;
+		for(int ele: x) {
+			sum += ele;
+		}		
+		System.out.println("SUM of Integer is: "+sum);		
+	}
+	
+	
+	public void m1(int name, int ...x) {
+		
 	}
 	
 	public static void main(String[] args) {
 		LaunchVarargs l = new LaunchVarargs();
-		l.add();
-		l.add(10, 30);
-		l.add(30, 40,50);
+		l.add(); // new int[]{}
+		l.add(10, 30); // new int[]{10, 30}
+		l.add(30, 40,50); //new int[]{30,40,50}
+		
+		l.addInt();
+		l.addInt(10, 30, 40, -50);
+		
+		
 
 	}
 
