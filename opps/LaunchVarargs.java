37139d8c8a927d869fa1d92d0e205e2f8a12c2f2
Mobin Arshad <mobink09@gmail.com>
2023-04-11 22:45:15 +0530

    Wrapper class

--- opps/LaunchVarargs.java
+++ opps/LaunchVarargs.java
@@ -18,10 +18,38 @@ public class LaunchVarargs {
 		System.out.println("SUM of Integer is: "+sum);		
 	}
 	
+	public void m1(int[] ...x) {
+		 for(int[] el: x ) {
+			 for(int a: el) {
+				 System.out.print(a+" ");
+			 }
+			 System.out.println();
+		 }
+	}
 	
-	public void m1(int name, int ...x) {
+	public void m1(String name, int ...x) {
 		
 	}
+	
+	public static void m2(long l) {
+		System.out.println("Long-Parameterized method");
+	}
+	
+	public static void m2(Integer i) {
+		System.out.println("Integer parameterized methods");
+	}
+	
+	public static void m3(Integer i) {
+		System.out.println("m3() Integer-parameterized method");
+	}
+	public static void m3(int ...x) {
+		System.out.println("m3() int var-args methods");
+	}
+	
+	public static void m4(Long l) {
+		System.out.println("Long parameterized");
+	}
+	
 	
 	public static void main(String[] args) {
 		LaunchVarargs l = new LaunchVarargs();
@@ -32,8 +60,14 @@ public class LaunchVarargs {
 		l.addInt();
 		l.addInt(10, 30, 40, -50);
 		
+		int[] a= {10, 20,30};
+		int[] b = {5, 8, 11};
 		
-
+		l.m1(a,b);
+		int c  = 20;
+		m2(c);
+        m3(c);
+        //m4(c); // CE:
 	}
 
 }
