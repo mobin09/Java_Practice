bc69534ceca4af4d90eb3916c5b9a97b30fd8114
Mobin Arshad <mobink09@gmail.com>
2023-05-03 09:00:02 +0530

    Concurrent collections

--- collection/coreJava/Demo1.java
+++ collection/coreJava/Demo1.java
@@ -1,8 +1,9 @@
 package collection.coreJava;
 
 import java.util.Enumeration;
+import java.util.Iterator;
 import java.util.Vector;
-
+import java.util.ArrayList;
 class Demo1 {
    public static void main(String[] args) {
 	   Vector v = new Vector();
@@ -16,6 +17,21 @@ class Demo1 {
 		   System.out.println(o);
 	   }
 	   
+	   ArrayList al = new ArrayList();
+	   al.add(10);
+	   al.add(20);
+	   al.add(30);
+	   
+//	   for(int i=0; i<al.size(); i++) {
+//		   System.out.println(al.get(i));
+//		   al.add(123);
+//	   }
+	   
+	   Iterator itr =  al.iterator();
+	   while(itr.hasNext()) {
+		   System.out.println(itr.next());
+		   al.add(143); // Fail-Fast (Concurrent Modificarion)
+	   }
 	   
    }
 }
