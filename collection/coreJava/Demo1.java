b00ac3e6a4d02ff3630d1befeb45417d14583b8e
Mobin Arshad <mobink09@gmail.com>
2023-05-03 21:21:52 +0530

    Collections

--- collection/coreJava/Demo1.java
+++ collection/coreJava/Demo1.java
@@ -5,6 +5,18 @@ import java.util.Iterator;
 import java.util.Vector;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.TreeSet;
+
+class Pg{
+	String pgName;
+	String area;
+	Pg(String pgName, String area){
+		this.pgName = pgName;
+		this.area = area;
+	}
+}
+
 class Demo1 {
    public static void main(String[] args) {
 	   Vector v = new Vector();
@@ -45,6 +57,47 @@ class Demo1 {
 		  Object ob = itrr.next();
 		  System.out.println(ob);
 		  coal.add(999);
-	  }   
+	  } 
+	  // 100, 50, 150, 25, 75, 125, 175
+	  TreeSet ts = new TreeSet();
+	  ts.add(100);
+	  ts.add(50);
+	  ts.add(150);
+	  ts.add(25);
+	  ts.add(75);
+	  ts.add(125);
+	  ts.add(175);
+	  System.out.println(ts);
+	  ArrayList al1 = new ArrayList();
+	  al1.add(100);
+	  al1.add(50);
+	  al1.add(150);
+	  al1.add(25);
+	  al1.add(75);
+	  al1.add(125);
+	  al1.add(175);
+	  System.out.println(al1);
+	  System.out.println(al1);
+	  Collections.sort(al1);
+	  System.out.println(al1);
+	  
+	  ArrayList<String> al2 = new ArrayList<>();
+	  al2.add("Mobin Arshad");
+	  al2.add("Rahmat Nagr");
+	  System.out.println(al2);
+	  
+	  Pg pg1 = new Pg("RaipurPG", "Secor-126");  
+	  Pg pg2 = new Pg("Virat Mansion", "Sector-125");
+	  ArrayList<Pg> alPg =  new ArrayList<>();
+	  alPg.add(pg1);
+	  alPg.add(pg2);
+	  System.out.println(alPg);
+	  
+	  Iterator<Pg> itr2 = alPg.iterator();
+	  while(itr2.hasNext()) {
+		  Pg p = (Pg)itr2.next();
+		  System.out.println("Pg Name:" + p.pgName + " Pg Address: " + p.area);
+	  }
+	  
    }
 }
