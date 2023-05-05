36537c19385bdd1114c10746a13f7e88a5d7a5a9
Mobin Arshad <mobink09@gmail.com>
2023-05-05 06:23:48 +0530

    Map

--- collection/coreJava/Demo2.java
+++ collection/coreJava/Demo2.java
@@ -2,11 +2,27 @@ package collection.coreJava;
 
 // Map<K, V>
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
 class Demo2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	    HashMap<Integer, String> hmap = new HashMap<>();	
+	    hmap.put(10, "Sachin");
+	    hmap.put(7, "MSD");
+	    hmap.put(18, "Kohli");
+	    System.out.println(hmap);
+	    
+	    LinkedHashMap lhm = new LinkedHashMap();
+	    lhm.put(30, "Mobin Arshad");
+	    lhm.put(50, "Washim Arshad");
+	    lhm.put(20, "Nadeem Arshad");
+	    lhm.put(10, "Jashim Arshad");
+	    
+	    System.out.println(lhm);
+	    
+    
 	}
 
 }
