d101d2cc750851b2f5185a551808bfc2fbc3d330
Mobin Arshad <mobink09@gmail.com>
2023-05-06 11:03:14 +0530

    Java 8 interface

---
+++ opps/LaunchInterf.java
@@ -0,0 +1,45 @@
+package opps;
+
+interface Interf1{
+	 void teaches();// all interface by default public and abstract
+	 void writeCodes();
+	 
+	 default void hi() { // participates in inheritance
+		 System.out.println("from java 8 onwards normal method is allowed");
+	 }
+	 
+	 static void ya() { // will not participate in inheritance
+		 System.out.println("with static method we can also create the method with implementation");
+	 }
+	 
+	 private void m1() { // java 9
+		 System.out.println("I am private method");
+	 }
+	 
+	 
+}
+
+
+class Students12 implements Interf1{
+	public void teaches() {
+		System.out.println("Mobin teaches the coding");
+	}
+	
+	public void writeCodes() {
+		System.out.println("Mobin Writes the code");
+	}
+	
+}
+
+
+public class LaunchInterf {
+
+	public static void main(String[] args) {
+		Students12 st1 = new Students12();
+		st1.hi();
+      
+		Interf1.ya(); 
+        
+	}
+
+}
