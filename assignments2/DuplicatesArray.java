f55d8cd477ab679e30644717b368467a0231d2ba
Mobin Arshad <mobink09@gmail.com>
2023-05-11 20:06:52 +0530

    sorting Assignments

---
+++ assignments2/DuplicatesArray.java
@@ -0,0 +1,16 @@
+public class DuplicatesArray {
+    public static void main(String[]args){
+        int[]arr=new int[]{1,2,3,4,2,5,6,8,8,3};
+        System.out.println("Duplicate elements in given array");
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]==arr[j]){
+                    System.out.println(arr[j]);
+                }
+
+            }
+        }
+
+    }
+    
+}
