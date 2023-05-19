5c1d3caf0650553ec6ddeac7280bad13f029fe64
Mobin Arshad <mobink09@gmail.com>
2023-05-19 20:25:02 +0530

    java Networking

--- multithreading/RDemo2.java
+++ multithreading/RDemo2.java
@@ -1,7 +1,7 @@
 package multithreading;
 
 class Display {
-	public synchronized void wish(String name) {
+	public synchronized  void wish(String name) {
 
 		for (int i = 0; i < 10; i++) {
 			System.out.println("Good Evening: " + name);
---
+++ networking/Demo1.java
@@ -0,0 +1,10 @@
+package networking;
+
+class Demo1 {
+
+	public static void main(String[] args) {
+		System.out.println("Java Networking starting");
+
+	}
+
+}
