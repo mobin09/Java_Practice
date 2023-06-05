acf28d187cc3b147f1e7b6a33ee4d168eb0c5f4b
Mobin Arshad <mobink09@gmail.com>
2023-06-05 22:55:33 +0530

    date

--- jdbc/Demo4.java
+++ jdbc/Demo4.java
@@ -1,4 +1,5 @@
 package jdbc;
+
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
@@ -6,9 +7,42 @@ import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Scanner;
+import java.sql.ResultSet;
+
 class Demo4 {
 
-	
+	public static void getCustomerData() {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			String query = "SELECT id, name, dob FROM customerDetails";
+			con = DBUtils.getConnection();
+			pstmt = con.prepareStatement(query);
+			rs = pstmt.executeQuery();
+			System.out.println("\tID \tNAME \t\t\tDOB");
+			while (rs.next()) {
+				int id = rs.getInt(1);
+				String name = rs.getString(2);
+				java.sql.Date d = rs.getDate(3);
+				System.out.println("\t" + id + "\t" + name + "\t" + d);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				con.close();
+				pstmt.close();
+				rs.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
 	public static void insertCustomerData() {
 		Connection con = null;
 		PreparedStatement pstmt = null;
@@ -17,7 +51,7 @@ class Demo4 {
 		String name = sc.nextLine();
 		System.out.println("Please enter your DOB(dd-MM-yy)");
 		String dob = sc.nextLine();
-		
+
 		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yy");
 		long milliseconds = 0l;
 		try {
@@ -26,9 +60,9 @@ class Demo4 {
 		} catch (ParseException e1) {
 			e1.printStackTrace();
 		}
-		
+
 		java.sql.Date sDate = new java.sql.Date(milliseconds);
-		
+
 		try {
 			String query = "INSERT INTO customerDetails (name, dob) values(?, ?)";
 			con = DBUtils.getConnection();
@@ -37,22 +71,23 @@ class Demo4 {
 			pstmt.setDate(2, sDate);
 			int updateRow = pstmt.executeUpdate();
 			System.out.println("Row Affected:" + updateRow);
-		} catch (SQLException e) {			
+		} catch (SQLException e) {
 			e.printStackTrace();
-		}finally {
+		} finally {
 			sc.close();
 			try {
 				con.close();
 				pstmt.close();
-				}catch(SQLException e) {
-					e.printStackTrace();
-				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
-		
+
 	}
-	
+
 	public static void main(String[] args) {
-		insertCustomerData();
+		//insertCustomerData();
+		getCustomerData();
 	}
 
 }
